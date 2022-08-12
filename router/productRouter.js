e17b2ae127b23518632eb2efe731a943dc6cdae6
kaushal sojitra <kaushalsojitra923@gmail.com>
2022-08-12 14:15:55 +0530

    update

--- router/productRouter.js
+++ router/productRouter.js
@@ -2,10 +2,6 @@ const express = require("express");
 const Route = express.Router();
 const Product = require("../model/productModel");
 
-Route.get("/",(req,resp)=> {
-    resp.render(<h1>Hello this is my first Heroku Project</h1>)
-})
-
 Route.post("/products", async (req, resp) => {
   try {
     const product = new Product(req.body);
